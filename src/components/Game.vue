d1e91f77a8a25411ab5bf10cc0a2f19ffdd0fbab
Hendra Susanto <benhanks040888@gmail.com>
2018-01-17 13:03:09 +0700

    fix built paths

--- src/components/Game.vue
+++ src/components/Game.vue
@@ -54,7 +54,7 @@ export default {
         return undefined
       }
 
-      return '../static/people/' + this.currentQuestion.image
+      return 'static/people/' + this.currentQuestion.image
     }
   },
   created () {
